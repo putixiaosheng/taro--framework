a65efde27298f451a9c96b0363008cc1baaadb4f
putixiaosheng <1130132722@qq.com>
2023-10-25 19:46:56 +0800

    second commit

---
+++ src/pages/homepage/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="homepage">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.css'
+
+export default {
+  setup () {
+    const msg = ref('Hello world3')
+    return {
+      msg
+    }
+  }
+}
+</script>
